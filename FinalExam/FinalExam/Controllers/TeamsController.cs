00bfb86aea6163d9bf34d8b852cb5705b9355391
Amandeep-saggu <aman007saggu@gmail.com>
2016-12-13 10:15:11 -0500

    comp2084-finalExaminaiton (commit 1)
    comp2084-finalExamination (commit 1)

---
+++ FinalExam/FinalExam/Controllers/TeamsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalExam.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private DatabaseContext db = new DatabaseContext();
+
+        //
+        // GET: /Teams/
+
+        public ActionResult Index()
+        {
+            var teams = db.Teams.Include(t => t.Division);
+            return View(teams.ToList());
+        }
+
+        //
+        // GET: /Teams/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        //
+        // GET: /Teams/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.DivisionID = new SelectList(db.Divisions, "DivisionID", "Name");
+            return View();
+        }
+
+        //
+        // POST: /Teams/Create
+
+        [HttpPost]
+        public ActionResult Create(Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Teams.Add(team);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DivisionID = new SelectList(db.Divisions, "DivisionID", "Name", team.DivisionID);
+            return View(team);
+        }
+
+        //
+        // GET: /Teams/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DivisionID = new SelectList(db.Divisions, "DivisionID", "Name", team.DivisionID);
+            return View(team);
+        }
+
+        //
+        // POST: /Teams/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(team).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DivisionID = new SelectList(db.Divisions, "DivisionID", "Name", team.DivisionID);
+            return View(team);
+        }
+
+        //
+        // GET: /Teams/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        //
+        // POST: /Teams/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Team team = db.Teams.Find(id);
+            db.Teams.Remove(team);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FinalExam/FinalExam/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FinalExam
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<Division> Divisions { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
